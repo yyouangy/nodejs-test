3ee8d953b9df19a0fb69f3c5843c93f219f9bfb1
kkygy <62780616+yyouangy@users.noreply.github.com>
2020-08-12 23:19:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var server = http.createServer(function(request, response) {
     if (path === '/') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`你好呀，袁国耀`)
+        response.write(`呵呵呵呵呵呵`)
         response.end()
     } else if (path === '/x') {
         response.statusCode = 200
